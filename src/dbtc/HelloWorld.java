b53202df8a96d59886faa6a76e9f2abd4c8abb1a
aban2002 <abandomingo@yahoo.com>
2016-04-19 15:28:06 -0700

    fixed build

--- src/dbtc/HelloWorld.java
+++ src/dbtc/HelloWorld.java
@@ -3,6 +3,6 @@ package dbtc;
 public class HelloWorld
 {
     public static void main(String[] args) {
-            System.out.println( :) jello stinks");;
+            System.out.println( :) jello stinks");
 	        }
 }
